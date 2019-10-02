56b0158ef40c2a97e65a3b06dd43afb781a3a07b
Anastassia Russak <46761351+Anastassia-88@users.noreply.github.com>
2019-10-02 16:33:16 +0200

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,30 +1,62 @@
 'use strict';
 
-// Getting a random integer between two values
-// The maximum is inclusive and the minimum is inclusive
-var getRandomIntInclusive = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+(function () {
+    var ESC_KEYCODE = 27;
+    var ENTER_KEYCODE = 13;
 
 
-// Getting a random item from array
-var getRandomItemFromArray = function (array) {
-  var min = 0;
-  var max = array.length;
-  return array[Math.floor(Math.random() * (max - min)) + min];
-};
+      // Getting a random integer between two values
+      // The maximum is inclusive and the minimum is inclusive
+      var getRandomIntInclusive = function (min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      };
+
+      // Getting a random item from array
+      var getRandomItemFromArray = function (array) {
+        return array[Math.floor(Math.random() * array.length)];
+      };
+
+      // Getting a random length array
+      var getRandomArray = function (array) {
+        var arrayLength = getRandomIntInclusive(0, array.length);
+        return array.slice(0, arrayLength);
+      };
+
+      // Getting the value of the selected option
+      var getSelectedOption = function (select) {
+          return select.options[select.selectedIndex].value;
+        };
+
+        var isEscEvent = function (evt, action) {
+          if (evt.keyCode === ESC_KEYCODE) {
+            action();
+          }
+        };
+
+      var isEnterEvent = function (evt, action) {
+        if (evt.keyCode === ENTER_KEYCODE) {
+          action();
+        }
+      };
+
+      // Disable/enable a form element
+      var switchFormElement = function (form, isDisabled) {
+        var elems = form.elements;
+        for (var i = 0; i < elems.length; i++) {
+          elems[i].disabled = isDisabled;
+        }
+      };
 
 
-// Getting a random length array
-var getRandomArray = function (array) {
-  var arrayLength = getRandomIntInclusive(0, array.length);
-  return array.slice(0, arrayLength);
-};
-
-
-// Getting the value of the selected option
-var getSelectedOption = function (select) {
-  return select.options[select.selectedIndex].value;
-};
+      window.util = {
+        getRandomIntInclusive: getRandomIntInclusive,
+        getRandomItemFromArray: getRandomItemFromArray,
+        getRandomArray: getRandomArray,
+        getSelectedOption: getSelectedOption,
+        isEscEvent: isEscEvent,
+        isEnterEvent: isEnterEvent,
+        switchFormElement: switchFormElement,
+      };
+    })();
