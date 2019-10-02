2b5dbb2e9c24d95d87fe0cdb4a82c07d29ca3347
Anastassia Russak <46761351+Anastassia-88@users.noreply.github.com>
2019-10-02 16:34:39 +0200

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,71 +1,68 @@
 'use strict';
 
-// Добавляет или удаляет аттрибут disabled у элементов формы
-var switchFormElement = function (form, isDisabled) {
-  var elems = form.elements;
-  for (var i = 0; i < elems.length; i++) {
-    elems[i].disabled = isDisabled;
-  }
-};
+(function () {
 
+  var filterForm = document.querySelector('.map__filters');
 
-// Getting address
-var getAddress = function () {
-  var pinX = mapPinMain.style.left;
-  var pinY = mapPinMain.style.top;
-  var addressX = parseInt(pinX, 10) + MAIN_PIN_WIDTH / 2;
-  var addressY;
+  var adForm = document.querySelector('.ad-form');
+  var roomsNumberSelect = adForm.querySelector('#room_number');
+  var guestsNumberSelect = adForm.querySelector('#capacity');
+  var priceInput = adForm.querySelector('#price');
+  var typeSelect = adForm.querySelector('#type');
+  var timeInSelect = adForm.querySelector('#timein');
+  var timeOutSelect = adForm.querySelector('#timeout');
+  var addressInput = adForm.querySelector('input[name=address]');
+  
 
-  if (map.classList.contains('map--faded')) { // Inactive mode
-    addressY = parseInt(pinY, 10) + MAIN_PIN_HEIGHT_INACTIVE / 2;
-  } else { // Active mode
-    addressY = parseInt(pinY, 10) + MAIN_PIN_HEIGHT_ACTIVE;
-  }
-  addressInput.value = Math.floor(addressX) + ', ' + Math.floor(addressY);
-};
+  // Checking capacity
+  var checkCapacity = function () {
+    var guestsNumberOptions = guestsNumberSelect.querySelectorAll('option');
+    var roomsNumber = parseInt(window.util.getSelectedOption(roomsNumberSelect), 10);
 
-// Checking capacity
-var checkCapacity = function () {
-  var guestsNumberOptions = guestsNumberSelect.querySelectorAll('option');
-  var roomsNumber = parseInt(getSelectedOption(roomsNumberSelect), 10);
+    for (var i = 0; i < guestsNumberOptions.length; i++) {
+      var guestsNumber = guestsNumberOptions[i];
 
-  for (var i = 0; i < guestsNumberOptions.length; i++) {
-    var guestsNumber = guestsNumberOptions[i];
-
-    if (roomsNumber >= parseInt(guestsNumber.value, 10) && (roomsNumber !== 100) && parseInt(guestsNumber.value, 10) !== 0) {
-      guestsNumber.disabled = false;
-    } else if (roomsNumber === 100 && parseInt(guestsNumber.value, 10) === 0) {
-      guestsNumber.disabled = false;
-      guestsNumber.selected = true;
-    } else {
-      guestsNumber.disabled = true;
-      guestsNumber.selected = false;
+      if (roomsNumber >= parseInt(guestsNumber.value, 10) && (roomsNumber !== 100) && parseInt(guestsNumber.value, 10) !== 0) {
+        guestsNumber.disabled = false;
+      } else if (roomsNumber === 100 && parseInt(guestsNumber.value, 10) === 0) {
+        guestsNumber.disabled = false;
+        guestsNumber.selected = true;
+      } else {
+        guestsNumber.disabled = true;
+        guestsNumber.selected = false;
+      }
     }
-  }
-};
+  };
 
-roomsNumberSelect.addEventListener('change', function () {
-  checkCapacity();
-});
+  roomsNumberSelect.addEventListener('change', function () {
+    checkCapacity();
+  });
 
-guestsNumberSelect.addEventListener('change', function () {
-  checkCapacity();
-});
-
-
-// Connecting accomodation type and min price
-typeSelect.addEventListener('change', function () {
-  var key = getSelectedOption(typeSelect);
-  priceInput.min = ACCOMMODATION_TYPE_NAME[key].minPrice;
-  priceInput.placeholder = ACCOMMODATION_TYPE_NAME[key].minPrice;
-});
+  guestsNumberSelect.addEventListener('change', function () {
+    checkCapacity();
+  });
 
 
-// Connecting time-in and time-out
-timeInSelect.addEventListener('change', function () {
-  timeOutSelect.value = timeInSelect.value;
-});
+  // Connecting accomodation type and min price
+  typeSelect.addEventListener('change', function () {
+    var key = window.util.getSelectedOption(typeSelect);
+    priceInput.min = ACCOMMODATION_TYPE_NAME[key].minPrice;
+    priceInput.placeholder = ACCOMMODATION_TYPE_NAME[key].minPrice;
+  });
 
-timeOutSelect.addEventListener('change', function () {
-  timeInSelect.value = timeOutSelect.value;
-});
+
+  // Connecting time-in and time-out
+  timeInSelect.addEventListener('change', function () {
+    timeOutSelect.value = timeInSelect.value;
+  });
+
+  timeOutSelect.addEventListener('change', function () {
+    timeInSelect.value = timeOutSelect.value;
+  });
+
+  window.form = {
+    filterForm: filterForm,
+    adForm: adForm,
+    addressInput: addressInput,
+  };
+})();
